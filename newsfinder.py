bb06c4f70ded1bff655d507270d1575af647c044
luthfiihakiim <18217018@std.stei.itb.ac.id>
2019-11-30 14:50:51 +0700

    fix api

--- newsfinder.py
+++ newsfinder.py
@@ -14,115 +14,16 @@ def getHeadlines( rss_url ):
 
     return headlines
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Welcome to News Feeder API</h1>
-<p>A prototype API for national and international news feed getter.</p>'''
-
-@app.route('/resources/documentation', methods=['GET'])
-def documentation():
-    return redirect('https://app.swaggerhub.com/apis/daffaadevvv/NewsFeederAPI/1.0.0', code = 303)
-
-
-@app.route('/resources/news/internasional', methods=['GET'])
-def indexinter():
-
-    # # A list to hold all headlines
-    # allinterheadlines = []
-    
-    # # List of RSS feeds that we will fetch and combine
-    # newsinturls = {
-    #     # 'rtnews':           'https://www.rt.com/rss/',
-    #     'googlenews':       'https://news.google.com/news/rss/?hl=en&amp;ned=us&amp;gl=US'
-    # }
-    
-    # # Iterate over the feed urls
-    # for key,url in newsinturls.items():
-    #     # Call getHeadlines() and combine the returned headlines with allheadlines
-    #     allinterheadlines.extend( getHeadlines( https://news.google.com/news/rss/?hl=en&amp;ned=us&amp;gl=US ) )
-
-    # #print(allinterheadlines)
-
-    # phrase = 'Trump'
-    # phrase = phrase.lower()
-    # index = 0
-    # results = []
-
-    # for interheadline in allinterheadlines:
-    #     if phrase in interheadline.lower():
-    #         results.append(interheadline)
-
-    # print("lalala")
-    # print(results)
-
-    # if re.search(r'\b' + re.escape(phrase) + r'\b', allinterheadlines):
-    #     print(index)
-    #     
-
-    return jsonify(getHeadlines('https://news.google.com/news/rss/?hl=en&amp;ned=us&amp;gl=US'))
-
-
-@app.route('/resources/news/dalamnegeri/{keyword}', methods=['GET'])
-def indexnat():
-
-    # A list to hold all headlines
-    allnatheadlines = []
-    
-    # List of RSS feeds that we will fetch and combine
-    newsnaturls = {
-        'republikautama':   'https://www.republika.co.id/rss',
-        'bbcindo':          'http://feeds.bbci.co.uk/indonesia/rss.xml',
-        'okezone':          'https://sindikasi.okezone.com/index.php/okezone/RSS2.0',
-        'detiknews':        'http://rss.detik.com/index.php/detikcom',
-        'suara':            'https://www.suara.com/rss',
-        'antaranews':       'https://www.antaranews.com/rss/news.xml'
-    }
-    
-    # Iterate over the feed urls
-    # for key,url in newsnaturls.items():
-    #     # Call getHeadlines() and combine the returned headlines with allheadlines
-    #     allnatheadlines.extend( getHeadlines( url ) )
-    
-    phrase = 'patimban'
-    phrase = phrase.lower()
-    # index = 0
-    results = []
-
-    for natheadline in allnatheadlines:
-        if phrase in natheadline.lower():
-            results.append(natheadline)
-    
-    print(results)
-
-    #print(allnatheadlines)
-
-    #return jsonify(allnatheadlines)
-    return jsonify(results)
-
-
 @app.route('/resources/news/dalam/<keyword>', methods=['GET'])
 def indexn(keyword):
     
-    # List of RSS feeds that we will fetch and combine
-    # newsnaturls = {
-    #     'republikautama':   'https://www.republika.co.id/rss',
-    #     'bbcindo':          'http://feeds.bbci.co.uk/indonesia/rss.xml',
-    #     'okezone':          'https://sindikasi.okezone.com/index.php/okezone/RSS2.0',
-    #     'detiknews':        'http://rss.detik.com/index.php/detikcom',
-    #     'suara':            'https://www.suara.com/rss',
-    #     'antaranews':       'https://www.antaranews.com/rss/news.xml'
-    # }
-    
-    # Iterate over the feed urls
-    
-        # Call getHeadlines() and combine the returned headlines with allheadlines
     print('data1')
     data = getHeadlines('https://www.republika.co.id/rss')
     print('data2')
     
     phrase = keyword
     phrase = phrase.lower()
-
+    
     print(phrase)
 
     result = {
@@ -134,9 +35,7 @@ def indexn(keyword):
         print(headline)
         if phrase in headline.lower():
             print('data3')
-            result = {
-                'link': headline
-            }
+            result['link'] = headline
             print(result)
             break
 
